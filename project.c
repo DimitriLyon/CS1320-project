2ef5964114c55b10e080360983fe6c405db710de
isperea11 <129994988+isperea11@users.noreply.github.com>
2023-04-23 20:04:00 -0600

    Update project.c

--- project.c
+++ project.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 
-int main(int argc, char **argv) {
+int menu(){
+    int option;
+
+	printf("\t\t\t-- Cruise Express --\n");
+	printf("\t\t-- Below are the avialable options --\n");
+    printf("1: Bahamas");
+    printf("2: Hawaii");
+    printf("3: Bermuda");
+    printf("4: Caribbean");
+    printf("5: Mexico");
+    printf("6: Galapagos");
+    printf("7: Alaska");
+    printf("8: European");
+    printf("9: Exit");
+
+    printf("Please enter the number associated with the available options");
+    scanf("%d", &option);
+    return option;
+}
+
+
+void main() {
 	
-	return 0;
+    int travel; //This will be where they are travelling
+    travel = menu(); //This will load up the main menu
+
+    switch(travel)
 }
